6df4f7211809a7c6d7db68f042f19ca7a5cb781c
kjk3217 <kjk3217@naver.com>
2025-11-22 00:03:07 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SiteProvider } from './SiteContext';
 import { Navbar, Footer } from './components/Layout';
 import { HeroSection } from './components/sections/HeroSection';
 import { WhyNSTSection } from './components/sections/WhyNSTSection';
@@ -8,25 +10,37 @@ import { ScientificSection } from './components/sections/ScientificSection';
 import { PortfolioSection } from './components/sections/PortfolioSection';
 import { ContactSection } from './components/sections/ContactSection';
 import { FloatingActions } from './components/FloatingActions';
+import { AdminPage } from './pages/Admin';
+
+const MainSite = () => (
+  <>
+    <Navbar />
+    <main>
+      <HeroSection />
+      <WhyNSTSection />
+      <ResultsSection />
+      <ProcessSection />
+      <ScientificSection />
+      <PortfolioSection />
+      <ContactSection />
+    </main>
+    <Footer />
+    <FloatingActions />
+  </>
+);
 
 const App = () => {
   return (
-    <div className="min-h-screen font-sans bg-white text-slate-900">
-      <Navbar />
-      
-      <main>
-        <HeroSection />
-        <WhyNSTSection />
-        <ResultsSection />
-        <ProcessSection />
-        <ScientificSection />
-        <PortfolioSection />
-        <ContactSection />
-      </main>
-
-      <Footer />
-      <FloatingActions />
-    </div>
+    <SiteProvider>
+      <BrowserRouter>
+        <div className="min-h-screen font-sans bg-white text-slate-900">
+          <Routes>
+            <Route path="/" element={<MainSite />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </SiteProvider>
   );
 };
 
