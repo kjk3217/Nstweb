d409463ed485070ae7e23d2cc91b856d6c7c55a5
kjk3217 <kjk3217@naver.com>
2025-11-23 00:32:45 +0900

    Update ResultsSection.tsx

--- src/components/sections/ResultsSection.tsx
+++ src/components/sections/ResultsSection.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { motion } from 'motion/react';
+import { useContent } from '../../context/ContentContext';
 
 export const ResultsSection = () => {
+  const { content } = useContent();
+  const data = content?.results || {};
+
   const stats = [
-    { value: "1,018+", label: "Complexes", sub: "Completed Projects" },
-    { value: "50+", label: "Teams", sub: "Professional Experts" },
-    { value: "20", label: "Years", sub: "Field Experience" }
+    { value: data.stat1Value, label: data.stat1Label, sub: data.stat1Sub },
+    { value: data.stat2Value, label: data.stat2Label, sub: data.stat2Sub },
+    { value: data.stat3Value, label: data.stat3Label, sub: data.stat3Sub }
   ];
 
   return (
@@ -25,7 +29,6 @@ export const ResultsSection = () => {
               transition={{ delay: index * 0.2 }}
               className="relative"
             >
-              {/* Circular Progress Background (Decorative) */}
               <div className="w-48 h-48 rounded-full border-4 border-white/10 mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10" />
               <div className="w-56 h-56 rounded-full border border-white/5 mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 -z-10 animate-[spin_10s_linear_infinite]" />
               
